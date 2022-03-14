be5e708d365b15411e07e2580d5204c11d828f64
jassonchaguala59 <62127213+jassonchaguala59@users.noreply.github.com>
2022-03-14 18:11:00 -0500

    Add files via upload
    prueba tecnica

---
+++ ServicioRepositoryTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Turnos.Infraestructure.Repositories;
+using Xunit;
+
+namespace Turnos.Test.Unit.Respositories
+{
+    public class ServicioRepositoryTest
+    {
+
+        [Fact]
+        public void GetAll()
+        {
+
+            // Arrange
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"ConnectionStrings:DefaultConnection", "Server=localhost\\SQLEXPRESS; Database=Turnos; Trusted_Connection=True; MultipleActiveResultSets=true"},
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+
+
+            ServicioRepository repository = new ServicioRepository(configuration);
+
+
+            // Act
+            var entitites = repository.GetAll();
+
+            // Assert            
+            Assert.True(entitites.Count > 0);
+        }
+    }
+}
---
+++ TurnoRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Turnos.Infraestructure.Repositories;
+using Xunit;
+
+namespace Turnos.Test.Unit.Respositories
+{
+    public class TurnoRepositoryTest
+    {
+
+        [Fact]
+        public void Generate()
+        {
+            // Arrange
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"ConnectionStrings:DefaultConnection", "Server=localhost\\SQLEXPRESS; Database=Turnos; Trusted_Connection=True; MultipleActiveResultSets=true"},
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+
+            ServicioRepository serviceRepository = new ServicioRepository(configuration);
+            TurnoRepository turnoRepository = new TurnoRepository(configuration);
+            var services = serviceRepository.GetAll();
+
+            DateTime fecha_inicio = Convert.ToDateTime("2021-01-16");
+            DateTime fecha_fin = Convert.ToDateTime("2021-01-31");
+            int id_servicio = services.LastOrDefault().Id_servicio;
+
+            // Act
+            var turnos = turnoRepository.Generate(fecha_inicio, fecha_fin, id_servicio);
+
+            // Assert
+            Assert.True(turnos.Count > 0);
+
+        }
+
+    }
+}
